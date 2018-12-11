471147616dfc3143296902b5fa8c0d9943c0f8b0
Kamiševs Fēlikss <s6_kamise_f@vea.lv>
2018-12-11 13:03:00 +0200

    WPF app created

---
+++ Calculator/ExpressionParser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+
+    // TODO clean up && debug
+    public class ExpressionParser
+    {
+        public ExpressionParser()
+        {
+        }
+
+        /**
+         * @param inputEquation
+         */
+        public double ExecuteStringEquation(string inputEquation)
+        {
+            List<string> expr = ToPosfix(inputEquation);
+            Stack<double> s = new Stack<double>();
+            double a, b, input;
+
+            foreach (string op in expr)
+                Console.WriteLine(op);
+
+            foreach (string str in expr)
+            {
+                if (!IsOperator(str))
+                {
+                    Double.TryParse(str.Replace(",", "."), out input);
+                    s.Push(input);
+                }
+                else
+                {
+                    if (s.Count > 1)
+                    {
+                        a = s.Pop();
+                        b = s.Pop();
+                        switch (str)
+                        {
+                            case "+":
+                                s.Push(b + a);
+                                break;
+                            case "-":
+                                s.Push(b - a);
+                                break;
+                            case "*":
+                                s.Push(b * a);
+                                break;
+                            case "/":
+                                if (a == 0) throw new ArithmeticException();
+                                s.Push(b / a);
+                                break;
+                            case "^":
+                                s.Push(Math.Pow(b, a));
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return s.Pop();
+        }
+
+        /**
+         * Convert infix to posfix expression
+         * 
+         * @param expression
+         * @return posfix expression as a List object of Strings
+         */
+        private List<string> ToPosfix(string expression)
+        {
+            List<string> posfix = new List<string>();
+            Stack<char> opstack = new Stack<char>();
+            int k;
+            bool lastOp = false;	// true if last substring was an operator
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == ' ')
+                    continue;
+
+                if (expression[i] >= '0' && expression[i] <= '9' || !lastOp && expression[i] == '-')
+                {
+                    string sb = "";
+                    k = i;
+                    while (k < expression.Length && (expression[k] >= '0' && expression[k] <= '9' || expression[k] == ',' || expression[k] == '.' || !lastOp && expression[i] == '-'))
+                    {
+                        sb += expression[k];
+                        i = k++;
+                    }
+
+                    posfix.Add(sb);
+                }
+
+                else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' || expression[i] == '^')
+                {
+                    if (!(opstack.Count == 0))
+                    {
+                        while (!StackIsEmpty(opstack) && Precedence(opstack.Peek()) <= Precedence(expression[i])
+                                && opstack.Peek() != '(')
+                            posfix.Add(char.ToString(opstack.Pop()));
+                    }
+                    lastOp = true;
+                    opstack.Push(expression[i]);
+                }
+                else if (expression[i] == '(')
+                {
+                    lastOp = false;
+                    opstack.Push(expression[i]);
+                }
+                else if (expression[i] == ')')
+                {
+                    while (!StackIsEmpty(opstack) && opstack.Peek() != '(')
+                        posfix.Add(char.ToString(opstack.Pop()));
+
+                    opstack.Pop();
+                }
+            }
+
+            while (!StackIsEmpty(opstack))
+                posfix.Add(char.ToString(opstack.Pop()));
+
+            return posfix;
+        }
+
+        /**
+         * Compute precedence
+         */
+        private int Precedence(char op)
+        {
+            if (op == '^')
+                return 1;
+            else if (op == '*' || op == '/')
+                return 2;
+            else
+                return 3;
+        }
+
+        /**
+         * Check if a char sequence is an operator
+         */
+        private bool IsOperator(string op)
+        {
+            return op.Equals("+") || op.Equals("-") || op.Equals("*") || op.Equals("/") || op.Equals("^");
+        }
+
+        /**
+         * Help function to check if stack of generic type T is empty
+         */
+        private bool StackIsEmpty<T>(Stack<T> s)
+        {
+            return s.Count == 0;
+        }
+
+    }
+}
+
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string inputText = "";  // this variable stores the equation that will be calculated
+
+        private ExpressionParser ep;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ep = new ExpressionParser();
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "0";
+            inputText += "0";
+        }
+
+        private void comma_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += ".";
+            inputText += ".";
+        }
+
+        /**
+         * Triggers the calcultation calling the expression parsing algorithm
+         */
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text = ep.ExecuteStringEquation(inputText).ToString();
+        }
+
+        private void subtract_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "-";
+            inputText += "-";
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "3";
+            inputText += "3";
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "2";
+            inputText += "2";
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "1";
+            inputText += "1";
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "+";
+            inputText += "+";
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "6";
+            inputText += "6";
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "5";
+            inputText += "5";
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "4";
+            inputText += "4";
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "*";
+            inputText += "*";
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "9";
+            inputText += "9";
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "8";
+            inputText += "8";
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "7";
+            inputText += "7";
+        }
+
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "/";
+            inputText += "/";
+        }
+
+        
+
+        private void rightP_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += ")";
+            inputText += ")";
+        }
+
+        private void leftP_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "(";
+            inputText += "(";
+        }
+
+        private void power_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "^";
+            inputText += "^";
+        }
+
+        private void powerTwo_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "^2";
+            inputText += "^2";
+        }
+
+        private void allClr_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text = "";
+            inputText = "";
+        }
+
+        private void sqroot_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Text += "^(1/2)";
+            inputText += "^(1/2)";
+
+        }
+
+        private void clr_Click(object sender, RoutedEventArgs e)
+        {
+            int len = mainTextBox.Text.Length;
+            StringBuilder sb = new StringBuilder(mainTextBox.Text);
+            sb[len - 1] = ' ';
+            mainTextBox.Text = sb.ToString().Trim();
+
+            inputText = mainTextBox.Text;
+        }
+
+        private void mainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void pltBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clrPlt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
